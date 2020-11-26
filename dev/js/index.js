9d88b780820319196c7b71cbb82d950a1db3fc18
Adrimava <adri_mv1@hotmail.com>
2020-11-26 19:58:05 +0100

    code norm polished

--- dev/js/index.js
+++ dev/js/index.js
@@ -10,6 +10,7 @@ let left = false;
 let canvas;
 let lienzo;
 let speed = 50; //lower is faster
+let	snakeSize = 3	;
 
 
 function  initialize()
@@ -17,22 +18,18 @@ function  initialize()
 	canvas = document.getElementById("canvas");
 	lienzo = canvas.getContext("2d");
 	document.addEventListener('keydown', choseDirection);
-  for (let r = 0; r < rows; r++)
-  {
-    map[r] = new Array(columns);
-  }
+  for (let i = 0; i < rows; i++)
+    map[i] = new Array(columns);
   for (let i = 0; i < rows; i++)
   {
     for (let j = 0; j < columns; j++)
     {
-		lienzo.strokeRect(j*canvas.width/rows,i*canvas.height/columns,canvas.width/rows,canvas.height/columns);
-		 map[i][j] = 0;
+			lienzo.strokeRect(j*canvas.width/rows,i*canvas.height/columns,canvas.width/rows,canvas.height/columns);
+			map[i][j] = 0;
 	  }
   }
-  for (let m = 0; m < maxSnake; m++)
-  {
-    snake[m] = new Array(2);
-  }
+  for (let i = 0; i < maxSnake; i++)
+    snake[i] = new Array(2);
   snake[0][0] = 5; //Initial position -Y- coordinate
   snake[0][1] = 5; //Initial position -X- coordinate
 }
@@ -86,23 +83,13 @@ function  snakeMovement()
 function  drawMovement()
 {
   for (let i = 0; i < rows; i++)
-  {
     for (let j = 0; j < columns; j++)
-    {
       lienzo.clearRect(j*canvas.width/rows,i*canvas.height/columns,canvas.width/rows+2,canvas.height/columns+2);
-    }
-  }
   for (let i = 0; i < rows; i++)
-  {
     for (let j = 0; j < columns; j++)
-    {
-      for (let k = 0; k < 30; k++)
-      {
+      for (let k = 0; k < snakeSize; k++)
         if (snake[k][0] == i && snake[k][1] == j)
           lienzo.fillRect(j*canvas.width/rows,i*canvas.height/columns,canvas.width/rows,canvas.height/columns);
-      }
-    }
-  }
 }
 
 window.addEventListener("load",initialize,false);
